c78a28f1cd83ef60e194b1c70b25773b49ef8d7c
DarylvdBerg <darylvdberg@gmail.com>
2026-07-05 16:54:26 +0200

    chore: remove test code

--- internal/thirdperson/Thirdpersonpoc.cs
+++ internal/thirdperson/Thirdpersonpoc.cs
@@ -8,15 +8,7 @@ public partial class Thirdpersonpoc : Node3D
     
     public override void _Ready()
     {
-        _detectionArea = GetNode<Area3D>("Area3D") ?? 
-            throw new ArgumentException("DetectionArea not found");
-
-        _detectionArea.BodyEntered += _OnBodyEntered;
-    }
-
-    private void _OnBodyEntered(Node3D body)
-    {
-        Log.Debug($"Body entered detection area: {body.Name}");
+        
     }
 
 }
